0de391ae68149c38d192e8226e929634bc692939
Rebaone Faith Matlaba <reba1faith26@gmail.com>
2025-01-03 15:20:36 +0200

    Commit For : Faith's First Commit
    Commit For : Faith's First Commit

---
+++ index.php
@@ -0,0 +1,32 @@
+
+<?php
+
+    //FAITH_COMMITS
+
+    //declaring variables
+
+    $string = "Faith";
+    $number = 25;
+    $double = 30.4;
+    $boolean = true;
+    $normal_array = array(2,4,6,8,10);
+    $associative_array = array("firstName"=>"Rebaone" , "lastName"=>"Matlaba");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>NWK Young System ISH</title>
+</head>
+<body>
+    
+    <h3> PHP BASICS </h3>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
